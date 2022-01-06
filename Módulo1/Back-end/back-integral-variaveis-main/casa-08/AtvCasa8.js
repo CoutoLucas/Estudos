363a62b72334ecf53f40c8102877d3702612f948
CoutoLucas <couto-lucasfonseca@hotmail.com>
2022-01-06 20:38:17 -0300

    Atividade Casa08

---
+++ Módulo1/Back-end/back-integral-variaveis-main/casa-08/AtvCasa8.js
@@ -0,0 +1,4 @@
+let Raio = 5, Comprimento = 0, Área = 0;
+Comprimento = 2 * Math.PI * Raio;
+Área = Math.PI * Math.pow(Raio, 2);
+console.log(`O Comprimento da circunferência é de ${Comprimento} cm e a Área é de ${Área} cm quadrados`);
