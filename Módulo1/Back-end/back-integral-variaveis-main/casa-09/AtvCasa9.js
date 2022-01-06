a29e9d8f25b978b71a04c9380e96dd73c5f93083
CoutoLucas <couto-lucasfonseca@hotmail.com>
2022-01-06 20:49:44 -0300

    Atividade casa09

---
+++ Módulo1/Back-end/back-integral-variaveis-main/casa-09/AtvCasa9.js
@@ -0,0 +1,5 @@
+let LadosDoPoligono = 5, SomaDosAngulos = 0, CadaAngulo = 0;
+SomaDosAngulos = (LadosDoPoligono - 2) * 180;
+console.log(`A soma dos angulos internos deste poligono é de ${SomaDosAngulos} graus`);
+CadaAngulo = SomaDosAngulos / LadosDoPoligono;
+console.log(`Caso todos os angulos internos tenham o mesmo tamanho, estes terão ${CadaAngulo} graus`);
