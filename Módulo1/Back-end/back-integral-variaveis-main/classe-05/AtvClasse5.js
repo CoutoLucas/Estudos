639e43c28f0b5d6b137fb3d868e079c8b117eb49
CoutoLucas <couto-lucasfonseca@hotmail.com>
2022-01-06 22:29:54 -0300

    Atividade de  Classe 5

---
+++ Módulo1/Back-end/back-integral-variaveis-main/classe-05/AtvClasse5.js
@@ -0,0 +1,3 @@
+let = x1 = 1, x2 = 1, y1 = 1, y2 = 4, distancia = 0;
+distancia = Math.sqrt(Math.pow(x1 - x2, 2) + (Math.pow(y1 - y2, 2)));
+console.log(`A distancia entre estes dois pontos é ${distancia.toFixed(2)} unidades de distancia`);
