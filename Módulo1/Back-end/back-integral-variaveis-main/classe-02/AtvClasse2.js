f2d712d06aaaf418f92a2e779831ea68d969acae
CoutoLucas <couto-lucasfonseca@hotmail.com>
2022-01-06 21:53:08 -0300

    Atividade de Classe2

---
+++ Módulo1/Back-end/back-integral-variaveis-main/classe-02/AtvClasse2.js
@@ -0,0 +1,3 @@
+let Celsius = 0, Farheinght = 80;
+Celsius = (Farheinght - 32) * (5/9);
+console.log(`A temperatura de ${Farheinght.toFixed(2)} graus Farheinght em Celsius é de ${Celsius.toFixed(2)} graus Celsius`);
