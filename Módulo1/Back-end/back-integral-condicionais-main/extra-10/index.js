5af2847ff720b6dc35063aedc8fcc3cd7cf7bb72
CoutoLucas <couto-lucasfonseca@hotmail.com>
2022-01-11 21:17:06 -0300

    Att do exercicio 10

--- Módulo1/Back-end/back-integral-condicionais-main/extra-10/index.js
+++ Módulo1/Back-end/back-integral-condicionais-main/extra-10/index.js
@@ -1,4 +1,4 @@
-const caractere = "c";
+const caractere = "!";
 
 if (caractere === "A") { console.log(`Vogal Maiuscula`) }
 else if (caractere === "E") { console.log(`Vogal Maiuscula`) }
@@ -10,5 +10,47 @@ else if (caractere === "e") { console.log(`Vogal Minuscula`) }
 else if (caractere === "i") { console.log(`Vogal Minuscula`) }
 else if (caractere === "o") { console.log(`Vogal Minuscula`) }
 else if (caractere === "u") { console.log(`Vogal Minuscula`) }
+else if (caractere === "b") { console.log(`Consoante Minuscula`) }
+else if (caractere === "c") { console.log(`Consoante Minuscula`) }
+else if (caractere === "d") { console.log(`Consoante Minuscula`) }
+else if (caractere === "f") { console.log(`Consoante Minuscula`) }
+else if (caractere === "g") { console.log(`Consoante Minuscula`) }
+else if (caractere === "h") { console.log(`Consoante Minuscula`) }
+else if (caractere === "j") { console.log(`Consoante Minuscula`) }
+else if (caractere === "k") { console.log(`Consoante Minuscula`) }
+else if (caractere === "l") { console.log(`Consoante Minuscula`) }
+else if (caractere === "m") { console.log(`Consoante Minuscula`) }
+else if (caractere === "n") { console.log(`Consoante Minuscula`) }
+else if (caractere === "p") { console.log(`Consoante Minuscula`) }
+else if (caractere === "q") { console.log(`Consoante Minuscula`) }
+else if (caractere === "r") { console.log(`Consoante Minuscula`) }
+else if (caractere === "s") { console.log(`Consoante Minuscula`) }
+else if (caractere === "t") { console.log(`Consoante Minuscula`) }
+else if (caractere === "v") { console.log(`Consoante Minuscula`) }
+else if (caractere === "w") { console.log(`Consoante Minuscula`) }
+else if (caractere === "x") { console.log(`Consoante Minuscula`) }
+else if (caractere === "y") { console.log(`Consoante Minuscula`) }
+else if (caractere === "z") { console.log(`Consoante Minuscula`) }
+else if (caractere === "B") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "C") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "D") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "F") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "G") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "H") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "J") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "K") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "L") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "M") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "N") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "P") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "Q") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "R") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "S") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "T") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "V") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "W") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "X") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "Y") { console.log(`Consoante Maiuscula`) }
+else if (caractere === "Z") { console.log(`Consoante Maiuscula`) }
 else if (caractere <= Infinity) { console.log(`Número`) }
-else { console.log(`Consoante`) };
+else { console.log(`Cacartere não definido`) };
