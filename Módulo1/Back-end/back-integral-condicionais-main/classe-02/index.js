283e36ac5f8c08dd8da2b4b00560773064902d88
Maurício <mauruciotaffarel@gmail.com>
2022-01-13 20:47:52 -0300

    Sugestão boa prática

--- Módulo1/Back-end/back-integral-condicionais-main/classe-02/index.js
+++ Módulo1/Back-end/back-integral-condicionais-main/classe-02/index.js
@@ -1,5 +1,5 @@
 const jogada1 = 5;
 const jogada2 = 2;
 const soma = jogada1 + jogada2;
-if (soma % 2 == 0) { console.log(`O jogador que escolheu par, é o vencedor`) }
+if (soma % 2 == 0) { console.log(`O jogador que escolheu par, é o vencedor`) } // Uma boa prática é usar ===
 else { console.log(`O jogador que escolheu impar, é o vencedor`) };
