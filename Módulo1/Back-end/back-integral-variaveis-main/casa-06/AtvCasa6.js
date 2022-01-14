7062738a070098c156e5180699e3f53d6546da8c
Maurício <mauruciotaffarel@gmail.com>
2022-01-13 21:04:35 -0300

    Idem

--- Módulo1/Back-end/back-integral-variaveis-main/casa-06/AtvCasa6.js
+++ Módulo1/Back-end/back-integral-variaveis-main/casa-06/AtvCasa6.js
@@ -1,4 +1,4 @@
 let distancia = 250, tempo = 20;
-VELOCIDADE = distancia / tempo;
+VELOCIDADE = distancia / tempo; // Idem
 VELOCIDADE = VELOCIDADE * 3.6;
 console.log(`A velocidade média neste percurso foi de ${VELOCIDADE} km/h`);
